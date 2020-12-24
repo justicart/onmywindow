11f1a78029144d99df95a8ba47a79ed06d5f5373
Justin Carter <justin@vidpresso.com>
2020-12-23 22:54:38 -0500

    mp4 src

--- src/components/Christmas.js
+++ src/components/Christmas.js
@@ -1,7 +1,13 @@
-import christmas from '../static/christmas.webm';
+import christmas from '../static/christmas.mp4';
+import christmas_webm from '../static/christmas.webm';
 
 function Christmas() {
-  return <video src={christmas} autoPlay loop muted autoBuffer playsInline />;
+  return (
+    <video autoPlay loop muted autoBuffer playsInline>
+      <source src={christmas} type="video/mp4" />
+      <source src={christmas_webm} type="video/webm" />
+    </video>
+  );
 }
 
 export default Christmas;
